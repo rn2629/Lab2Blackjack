cdeb31ef8c86c39add19e4ecb7be29360a02efce
Rodrigue Nalaur Ngampiep Manfouo <NgaRo1830511@etu.cegepjonquiere.ca>
2018-10-17 12:35:23 -0400

    Ajoutez des fichiers projet.

---
+++ Lab2Blackjack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
